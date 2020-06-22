a36a3b105b19f1d297b2a09986850915fb663add
Tanya Hodges <tanyac52003@gmail.com>
2020-06-22 17:31:39 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -5,13 +5,17 @@ def highest_student_gpa
 end
 
 def lowest_student_gpa
+  "SELECT MIN(gpa) FROM students"
 end
 
 def average_student_gpa
+  "AVG(gpa) FROM students"
 end
 
 def total_tardies_for_all_students
+  "COUNT(tardies) FROM students"
 end
 
 def average_gpa_for_9th_grade
+  ""
 end
